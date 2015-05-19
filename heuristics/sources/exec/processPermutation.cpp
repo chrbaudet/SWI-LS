45de7992bafcb8870ba314dff3c36d2e31a49639
baudet <christian.baudet@inria.fr>
2015-05-20 00:46:05 +0200

    User can select heuristics in processPermutation

--- heuristics/sources/exec/processPermutation.cpp
+++ heuristics/sources/exec/processPermutation.cpp
@@ -25,10 +25,15 @@
 #include <heuristics/heuristics.hpp>
 #include <permutation/permutation.hpp>
 
+#define NHEURISTICS 7
+
 /* ************************************************************************** */
 // Struct to receive the command line parameters
 struct Parameters {
+  // Input permutation
   Permutation permutation;
+  // List of heuristics to be considered
+  std::vector<bool> h;
 };
 /* ************************************************************************** */
 
@@ -36,10 +41,19 @@ struct Parameters {
 // Prints program usage
 void printUsage() {
 
-  std::cerr << std::endl << "Usage: processPermutation <s> <p>" << std::endl << std::endl;
+  std::cerr << std::endl << "Usage: processPermutation <s> <p> [h]" << std::endl << std::endl;
 
   std::cerr << "  <s>\t0 = unsigned permutations or 1 = signed permutations." << std::endl;
   std::cerr << "  <p>\tPermutation." << std::endl;
+  std::cerr << "  [h]\tList of heuristic identifiers separated by comma." << std::endl;
+  std::cerr << "     \tOptional parameter. Possible identifiers:" << std::endl;
+  std::cerr << "\t\t1 - LR" << std::endl;
+  std::cerr << "\t\t2 - SMP" << std::endl;
+  std::cerr << "\t\t3 - NB+LR" << std::endl;
+  std::cerr << "\t\t4 - NB+STRIP" << std::endl;
+  std::cerr << "\t\t5 - NB+BESTSTRIP" << std::endl;
+  std::cerr << "\t\t6 - NB+LRSTRIP" << std::endl;
+  std::cerr << "\t\t7 - NB+SMP" << std::endl;
 
   std::cerr << " ----------------------------------------------------------------------" << std::endl;
   std::cerr << " |This program processes the given permutation accordingly with the   |" << std::endl;
@@ -50,15 +64,7 @@ void printUsage() {
   std::cerr << " ----------------------------------------------------------------------" << std::endl << std::endl;
 
   std::cerr << " ----------------------------------------------------------------------" << std::endl;
-  std::cerr << " |The given permutation is processed by the following heuristics:     |" << std::endl;
-  std::cerr << " |  1 - LR                                                            |" << std::endl;
-  std::cerr << " |  2 - SMP                                                           |" << std::endl;
-  std::cerr << " |  3 - NB+LR                                                         |" << std::endl;
-  std::cerr << " |  4 - NB+STRIP                                                      |" << std::endl;
-  std::cerr << " |  5 - NB+BESTSTRIP                                                  |" << std::endl;
-  std::cerr << " |  6 - NB+LRSTRIP                                                    |" << std::endl;
-  std::cerr << " |  7 - NB+SMP                                                        |" << std::endl;
-  std::cerr << " |                                                                    |" << std::endl;
+  std::cerr << " |The given permutation is processed by the selected heuristics       |" << std::endl;
   std::cerr << " |For each heuristic, the sequence of inversions and its total weight.|" << std::endl;
   std::cerr << " ----------------------------------------------------------------------" << std::endl << std::endl;
 
@@ -71,11 +77,12 @@ void printUsage() {
 // Verifies the list of arguments
 Parameters processArguments(int argc, char* argv[]) {
 
-  if (argc != 3) printUsage();
+  if (argc != 3 && argc != 4) printUsage();
 
   bool error = false;
 
   Parameters toReturn;
+  toReturn.h = std::vector<bool>(NHEURISTICS);
 
   // Signed/Unsigned
   bool sign = std::string(argv[1]).compare("1") == 0;
@@ -109,6 +116,55 @@ Parameters processArguments(int argc, char* argv[]) {
 
   toReturn.permutation = Permutation(permutation, sign);
 
+  // Heuristics to be considered
+  for (integer index = 0; index < NHEURISTICS; ++index) {
+    toReturn.h[index] = false;
+  }
+
+  integer nHeuristics = 0;
+  if (argc == 4) {
+    std::string aux = std::string(argv[3]);
+    size_t index = 0;
+    size_t length = aux.length();
+    size_t comma = aux.find_first_of(",");
+    try {
+      if (comma != std::string::npos) {
+	while (index < length) {
+	  integer id = std::stoi(aux.substr(index, (comma - index)));
+	  if (id < 1 || id > 7) {
+	    error = true;
+	    break;
+	  }
+	  toReturn.h[id - 1] = true;
+	  index = comma + 1;
+	  comma = aux.find_first_of(",", index);
+	  if (comma == std::string::npos) comma = length;
+	  nHeuristics++;
+	}
+      } else {
+	integer id = std::stoi(aux.substr(0, comma));
+	if (id < 1 || id > 7) {
+	  error = true;
+	}
+	toReturn.h[id - 1] = true;
+	nHeuristics++;
+      }
+    } catch (const std::exception& ia) {
+      error = true;
+    }
+    if (error) {
+      std::cerr << std::endl << "ERROR!!! Invalid heuristic identifier." << std::endl;
+      printUsage();
+    }
+  }
+
+  if (argc != 4 || nHeuristics == 0) {
+    // Include all heuristics
+    for (integer index = 0; index < NHEURISTICS; ++index) {
+      toReturn.h[index] = true;
+    }
+  }
+
   return toReturn;
 }
 /* ************************************************************************** */
@@ -124,49 +180,49 @@ void process(const Parameters parameters) {
   std::cout << "------------------------------------------------------" << std::endl;
 
   for (integer h = 1; h <= 7; ++h) {
-
-    switch (h) {
-    case LR:
-      std::cout << "LR           : ";
-      break;
-    case SMP:
-      std::cout << "SMP          : ";
-      break;
-    case NB_PLUS_LR:
-      std::cout << "NB+LR        : ";
-      break;
-    case NB_PLUS_STRIP:
-      std::cout << "NB+STRIP     : ";
-      break;
-    case NB_PLUS_BESTSTRIP:
-      std::cout << "NB+BESTSTRIP : ";
-      break;
-    case NB_PLUS_LRSTRIP:
-      std::cout << "NB+LRSTRIP   : ";
-      break;
-    case NB_PLUS_SMP:
-      std::cout << "NB+SMP       : ";
-      break;
-    }
-
-    integer weight = 0;
-    Inversions inversions = Heuristics::sort(pi, problem, h, weight);
-    if (weight < 0) {
-      std::cout << "Loop or heuristic error." << std::endl;
-    } else {
-      std::cout << weight << std::endl;
-    }
-
-    int nInversions = inversions.size();
-    if (nInversions > 0) {
-      std::cout << inversions[0];
-      for (integer index = 1; index < nInversions; ++index) {
-	std::cout << std::endl << inversions[index];
+    if (parameters.h[h - 1]) {
+      switch (h) {
+      case LR:
+	std::cout << "LR           : ";
+	break;
+      case SMP:
+	std::cout << "SMP          : ";
+	break;
+      case NB_PLUS_LR:
+	std::cout << "NB+LR        : ";
+	break;
+      case NB_PLUS_STRIP:
+	std::cout << "NB+STRIP     : ";
+	break;
+      case NB_PLUS_BESTSTRIP:
+	std::cout << "NB+BESTSTRIP : ";
+	break;
+      case NB_PLUS_LRSTRIP:
+	std::cout << "NB+LRSTRIP   : ";
+	break;
+      case NB_PLUS_SMP:
+	std::cout << "NB+SMP       : ";
+	break;
       }
+
+      integer weight = 0;
+      Inversions inversions = Heuristics::sort(pi, problem, h, weight);
+      if (weight < 0) {
+	std::cout << "Loop or heuristic error." << std::endl;
+      } else {
+	std::cout << weight << std::endl;
+      }
+
+      int nInversions = inversions.size();
+      if (nInversions > 0) {
+	std::cout << inversions[0];
+	for (integer index = 1; index < nInversions; ++index) {
+	  std::cout << std::endl << inversions[index];
+	}
+      }
+
+      std::cout << std::endl << "------------------------------------------------------" << std::endl;
     }
-
-    std::cout << std::endl << "------------------------------------------------------" << std::endl;
-
   }
 
 }
